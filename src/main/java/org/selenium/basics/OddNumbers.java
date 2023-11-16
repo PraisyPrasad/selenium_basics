649f9be1871a5dbf15711714c82a4fe60dcb82bd
PraisyPrasad <praisyprasad123@gmail.com>
2023-11-16 09:14:18 +0530

    odd numbers

---
+++ src/main/java/org/selenium/basics/OddNumbers.java
@@ -0,0 +1,12 @@
+package org.selenium.basics;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=1;i<=10;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
