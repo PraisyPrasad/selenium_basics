49fc62ddc69aee01909c958856006f94ee67cf92
PraisyPrasad <praisyprasad123@gmail.com>
2023-11-23 22:22:21 +0530

    initial commit

--- src/main/java/org/selenium/basics/EdgeBrowserLaunch.java
+++ src/main/java/org/selenium/basics/EdgeBrowserLaunch.java
@@ -7,5 +7,15 @@ public class EdgeBrowserLaunch {
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver();
         driver.get("https://demo.guru99.com/test/newtours/");
+        driver.manage().window().maximize();
+        String pageTitle=driver.getTitle();
+        System.out.println(pageTitle);
+        String currentUrl=driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        String handel=driver.getWindowHandle();
+        System.out.println(handel);
+        String sourceCode=driver.getPageSource();
+        System.out.println(sourceCode);
+        driver.close();
     }
 }
